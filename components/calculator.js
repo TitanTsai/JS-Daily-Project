f9dfcefc626716a6e598050cc708c5eec3d1ca00
TitanTsai <71091416+TitanTsai@users.noreply.github.com>
2021-04-13 13:18:12 +0800

    structure revised

--- components/calculator.js
+++ components/calculator.js
@@ -1,6 +1,27 @@
-const app=Vue.createApp({})
-
 app.component('simple-calculator',{
+    template:
+    `<div class="calculator_container" id="calculator">
+        <div class="calculator_output">{{current || 0}}</div>
+        <div class="calculator_btn misc" @click="clear">C</div>
+        <div class="calculator_btn misc" @click="sign" >+/-</div>
+        <div class="calculator_btn misc" @click="percentage">%</div>
+        <div class="calculator_btn operators" @click="devide">÷</div>
+        <div class="calculator_btn" @click="append(7)">7</div>
+        <div class="calculator_btn" @click="append(8)">8</div>
+        <div class="calculator_btn" @click="append(9)">9</div>
+        <div class="calculator_btn operators" @click="times">×</div>
+        <div class="calculator_btn" @click="append(4)">4</div>
+        <div class="calculator_btn" @click="append(5)">5</div>
+        <div class="calculator_btn" @click="append(6)">6</div>
+        <div class="calculator_btn operators" @click="minus">-</div>
+        <div class="calculator_btn" @click="append(1)">1</div>
+        <div class="calculator_btn" @click="append(2)">2</div>
+        <div class="calculator_btn" @click="append(3)">3</div>
+        <div class="calculator_btn operators" @click="plus">+</div>
+        <div class="calculator_btn zero" @click="append(0)">0</div>
+        <div class="calculator_btn" @click="dot">.</div>
+        <div class="calculator_btn operators" @click="equals">=</div>
+    </div>`,
     data(){
         return{
             current:'',
@@ -60,33 +81,5 @@ app.component('simple-calculator',{
             this.previous='';
             
         },
-        
-        
     },
-    template:
-    `<div class="calculator_container" id="calculator">
-        <div class="calculator_output">{{current || 0}}</div>
-        <div class="calculator_btn misc" @click="clear">C</div>
-        <div class="calculator_btn misc" @click="sign" >+/-</div>
-        <div class="calculator_btn misc" @click="percentage">%</div>
-        <div class="calculator_btn operators" @click="devide">÷</div>
-        <div class="calculator_btn" @click="append(7)">7</div>
-        <div class="calculator_btn" @click="append(8)">8</div>
-        <div class="calculator_btn" @click="append(9)">9</div>
-        <div class="calculator_btn operators" @click="times">×</div>
-        <div class="calculator_btn" @click="append(4)">4</div>
-        <div class="calculator_btn" @click="append(5)">5</div>
-        <div class="calculator_btn" @click="append(6)">6</div>
-        <div class="calculator_btn operators" @click="minus">-</div>
-        <div class="calculator_btn" @click="append(1)">1</div>
-        <div class="calculator_btn" @click="append(2)">2</div>
-        <div class="calculator_btn" @click="append(3)">3</div>
-        <div class="calculator_btn operators" @click="plus">+</div>
-        <div class="calculator_btn zero" @click="append(0)">0</div>
-        <div class="calculator_btn" @click="dot">.</div>
-        <div class="calculator_btn operators" @click="equals">=</div>
-    </div>`
 })
-
-
-app.mount('#calculator')
