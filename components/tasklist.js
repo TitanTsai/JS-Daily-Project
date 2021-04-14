62233e8aad4e91d3f3edcc33bef203a801befd7d
TitanTsai <71091416+TitanTsai@users.noreply.github.com>
2021-04-14 20:34:05 +0800

    category function

--- components/tasklist.js
+++ components/tasklist.js
@@ -4,27 +4,24 @@ app.component('task-list',{
             <div class="task-header">
                 <div style="width:75%">
                     <input class="task-input" type="text" v-model.trim="newtask" @keyup.enter="addtask" placeholder="Write Something">
-                    <div style="margin-top:24px">
+                    <div style="margin-top:1em">
                         <select class="task-catagory" v-model="catagory">
-                            <option value="null" disabled selected>catagory</option>
-                            <option>business</option>
-                            <option>personal</option>
+                            <option value="" selected disabled hidden>catagory</option>
+                            <option v-for="type in types">{{type.typename}}</option>
                         </select>
                         <input class="date-selector" placeholder="due date" v-model="duedate"></input>
                     </div>
                 </div>
                 <div class="task-add"  @click="addtask()">+</div>
-                
             </div>
-            <div v-for="(task,index) in tasklist" :key="task.index">
-                <div class="task-item">
-                    <div class="task-name-group">
-                        <input type="checkbox" :id="task.id" v-model="task.isdone">
-                        <label :for="task.id" class="task-name" :class="{isdone:task.isdone}" ><span></span>{{task.taskname}}</label>
-                    </div>
-                    <p class="task-date">{{task.duedate}}</p>
-                    <div class="remove_btn" @click="removetask(index)">X</div>
+            
+            <div class="task-item" v-for="(task,index) in tasklist" :key="task.index">
+                <div class="task-name-group">
+                    <input type="checkbox" :id="task.id" v-model="task.isdone">
+                    <label :for="task.id" class="task-name" :class="{isdone:task.isdone}"><span></span>{{task.taskname}}</label>
                 </div>
+                <p class="task-date">{{task.duedate}}</p>
+                <div class="remove_btn" @click="removetask(index)">X</div>
             </div>
         </div>
         <p style="color:#fff">{{tasklist}}</p>
@@ -40,6 +37,7 @@ app.component('task-list',{
                 'isdone':false},
            ],
            newtask:'',
+           types:[{'typename':'business','color':'#524EEE'},{'typename':'personal','color':'#6FCF97'}]
         }
     },
     methods:{
@@ -50,7 +48,6 @@ app.component('task-list',{
 
             this.tasklist.unshift({'id':this.tasklist.length ,'taskname':this.newtask,'catagory':this.catagory,'duedate':this.duedate,'isdone':false})
             this.newtask=''
-            this.catagory=''
             this.duedate=''
         },
         removetask(index){
@@ -58,5 +55,6 @@ app.component('task-list',{
         }
     },
     computed:{
+
         }
 })
