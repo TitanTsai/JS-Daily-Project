358a3c1e625343d121a543f4a467fe17c84a072c
TitanTsai <71091416+TitanTsai@users.noreply.github.com>
2021-04-19 19:53:17 +0800

    carousel function

--- components/carousel.js
+++ components/carousel.js
@@ -1,33 +1,48 @@
 app.component('imageSlider',{
     template:`
-    <div class="image-slider" v-for="i in [images]">    
+    <div class="image-slider">
         <div class="image-slider--prev" @click="changeIndex(-1)">&#10094;</div>
-        <img class="image-slide--image" :src="currentImage">
+       
+            <div v-for="(item,i) in images" :key="item.city">
+                <transition name="fade">
+                    <img class="image-slider--image" v-if="index===i"  :src="item.src">
+                </transition>
+            </div>
+
         <div class="image-slider--next" @click="changeIndex(1)">&#10095;</div>
     </div>
     `,
     data(){
         return{
             images:[
-                "./assets/images/us-losangel.jpg",
-                "./assets/images/as-osaka.jpg",
-                "./assets/images/eu-frankfurt.jpg",
-                "./assets/images/eu-london.jpg",
+                {"city":"Los Angel","src":"./assets/images/us-losangel.jpg"},
+                {"city":"Osaka","src":"./assets/images/as-osaka.jpg"},
+                {"city":"Frankfurt","src":"./assets/images/eu-frankfurt.jpg"},
+                {"city":"London","src":"./assets/images/eu-london.jpg"}
             ],
-            currentIndex:0,
+            index:0,
+            timer:null,
         }
     },
-    mounted: function(){
-    
+    mounted(){
+        //this.autoPlay();
+        //console.log('mounted')
     },
     methods:{
+        autoPlay(){
+            this.timer = setInterval(()=>{this.changeIndex(1)},5000)
+        },
+        
         changeIndex(value){
-            this.currentIndex = (this.currentIndex + value + this.images.length) % this.images.length
+            this.index = (this.index + value + this.images.length) % this.images.length;
+        },
+
+        resetTimer(){
+            clearInterval(this.timer);
+            this.autoPlay();
         }
     },
     computed:{
-        currentImage(){
-            return this.images[this.currentIndex]
-        }
+
     }
 })
